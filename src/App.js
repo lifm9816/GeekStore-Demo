127943a4cfe484f2edcc57e3cdd5c8e67b4083e1
Luis Martínez <lifm9816@gmail.com>
2023-10-26 23:01:23 -0600

    Prueba barra de navegación móvil 5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import MobileNav from './Components/Mobile-Nav';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
 import MyCarousel from './Components/Carousel';
+import MobileNav from './Components/Mobile-Nav';
 
 function App() {
 
   return (
     <div>
-      
-        <Header />
-        <MyCarousel />
-        
-        <MobileNav />
+      <Header />
+      <MyCarousel />
+      <MobileNav />
     </div>
   );
 }
