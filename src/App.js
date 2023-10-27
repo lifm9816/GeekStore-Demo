148f677393249d715e2a5ba994da3c346ac961f8
Luis Martínez <lifm9816@gmail.com>
2023-10-26 23:03:13 -0600

    Correción :(

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import MyCarousel from './Components/Carousel';
 import MobileNav from './Components/Mobile-Nav';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
 
   return (
-    <div>
+    
+    <Router>
       <Header />
-      <MyCarousel />
+      
+      <Routes>
+        <Route path = "/" element = {<Home/>} />
+        <Route path = "/login" element = {<Login />} />
+      </Routes>
       <MobileNav />
-    </div>
+    </Router>
   );
 }
 
